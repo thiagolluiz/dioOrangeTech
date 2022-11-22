49da3b303ef700d0ec4663663248341496f5419b
thiagolluiz <delimaluiz.thiago@gmail.com>
2022-11-22 20:50:28 -0300

    deploy

---
+++ primeirosPassosComJavascript/aula.js
@@ -0,0 +1,7 @@
+
+let precoCombust = 5.5;
+let consumoMedioCarro = 10;
+let distPercorrida = 150;
+
+
+console.log('Valor gasto de combustivel é: '+ (distPercorrida/consumoMedioCarro*precoCombust) + ' reais');
