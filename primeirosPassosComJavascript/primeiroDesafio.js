45d723320d3f5551eada317fe85b68caa24cd09c
thiagolluiz <delimaluiz.thiago@gmail.com>
2022-11-29 21:54:03 -0300

    Deploy

--- primeirosPassosComJavascript/primeiroDesafio.js
+++ primeirosPassosComJavascript/primeiroDesafio.js
@@ -2,6 +2,14 @@
 let precoCombust = 5.79;
 let consumoMedioCarro = 12;
 let distPercorrida = 1580;
+let gastoComb = distPercorrida/consumoMedioCarro
 
 
-console.log('Valor gasto de combustivel é: '+ (distPercorrida/consumoMedioCarro*precoCombust).toFixed(2) + ' reais');
+console.log('Valor gasto de combustivel é: '+ (gastoComb*precoCombust).toFixed(2) + ' reais');
+
+
+let precoGas = 4.79;
+let precoAlc = 3.59;
+
+console.log('Valor gasto de gasolina é: '+ (gastoComb*precoGas).toFixed(2) + ' reais');
+console.log('Valor gasto de gasolina é: '+ (gastoComb*precoAlc).toFixed(2) + ' reais');
